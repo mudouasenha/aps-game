2992f29348c317c028dd767cebace8a6804d272c
samuelguilan <samuel.landre@hotmail.com>
2019-09-08 14:32:16 -0300

    Continuando a interface grafica

--- JogoDuvido/src/InterfaceGrafica/InterfaceMao.java
+++ JogoDuvido/src/InterfaceGrafica/InterfaceMao.java
@@ -1,7 +1,22 @@
 package InterfaceGrafica;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import java.awt.Color;
 
 public class InterfaceMao extends JPanel{
-
+	public InterfaceMao() {
+		setBackground(new Color(25, 25, 112));
+		setLayout(null);
+		
+		JButton btnDeclarar = new JButton("DECLARAR");
+		btnDeclarar.setBounds(605, 12, 102, 76);
+		add(btnDeclarar);
+		
+		JList list = new JList();
+		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		list.setBounds(12, 12, 581, 76);
+		add(list);
+	}
 }
--- JogoDuvido/src/InterfaceGrafica/InterfaceMesa.java
+++ JogoDuvido/src/InterfaceGrafica/InterfaceMesa.java
@@ -8,6 +8,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class InterfaceMesa extends JFrame{
 	public InterfaceMesa() {
@@ -26,24 +27,44 @@ public class InterfaceMesa extends JFrame{
 		menuBar.add(mntmIniciarPartida);
 		getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
+		JPanel panel = new InterfaceMao();
 		panel.setBounds(0, 329, 719, 100);
 		getContentPane().add(panel);
-		
-		JLabel lblJogador = new JLabel("Jogador2");
-		lblJogador.setBounds(48, 41, 66, 15);
-		getContentPane().add(lblJogador);
-		
-		JLabel lblJogador_1 = new JLabel("Jogador3");
-		lblJogador_1.setBounds(599, 41, 66, 15);
-		getContentPane().add(lblJogador_1);
 		
 		JButton btnDuvido = new JButton("DUVIDO!");
 		btnDuvido.setBounds(269, 292, 114, 25);
 		getContentPane().add(btnDuvido);
 		
 		JLabel lblMonte = new JLabel("Monte");
-		lblMonte.setBounds(292, 139, 66, 15);
+		lblMonte.setBounds(317, 136, 66, 15);
 		getContentPane().add(lblMonte);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.YELLOW);
+		panel_1.setBounds(12, 12, 133, 169);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNomeadversario = new JLabel("nomeAdversario1");
+		lblNomeadversario.setBounds(12, 12, 109, 22);
+		panel_1.add(lblNomeadversario);
+		
+		JLabel lblImgcartas = new JLabel("imgCartas");
+		lblImgcartas.setBounds(12, 57, 70, 100);
+		panel_1.add(lblImgcartas);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.RED);
+		panel_2.setBounds(574, 12, 133, 169);
+		getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNomeadversario_1 = new JLabel("nomeAdversario2");
+		lblNomeadversario_1.setBounds(12, 12, 109, 22);
+		panel_2.add(lblNomeadversario_1);
+		
+		JLabel label = new JLabel("imgCartas");
+		label.setBounds(51, 57, 70, 100);
+		panel_2.add(label);
 	}
 }
